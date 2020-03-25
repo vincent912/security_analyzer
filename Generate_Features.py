8cbe5c3a9e459005ca013d480787f73e66bfafc6
Vincent Li <vincentli@Vincents-MBP.attlocal.net>
2020-03-25 13:19:45 -0700

    udpates to Generate_Features.py

--- Generate_Features.py
+++ Generate_Features.py
@@ -2,10 +2,17 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
 import pandas as pd
+import argparse
+import sys
 
+
+
+#Defines how many days to use to calculate stdevs
 STD_Period = 20
 RSI_Periods = 14
 
+#The ticker symbol of the security to be analyzed.
+ticker = ""
 
 def check20SMA(df):
     cols = df.columns.values
@@ -16,6 +23,7 @@ def check20SMA(df):
         add20SMA(df)
         return df.shape[1]-1
 
+#Adds the Relative Strength Indicator
 def addRSI(df):
     gains = 0
     losses = 0
@@ -43,7 +51,7 @@ def addRSI(df):
         RS = gains/losses
         df.iloc[i, df.shape[1]-1] = 100 - (100/(1+RS))
 
-
+#Adds a 20 day simple moving average.
 def add20SMA(df):
     df.loc[:,"20SMA"] = 0
     for i in range(19,len(df)):
@@ -51,7 +59,8 @@ def add20SMA(df):
         for j in range(20):
             sum20 += df.iloc[i-19+j, 0]
         df.iloc[i, df.shape[1]-1] = sum20/20
-            
+
+#Adds bolinger bands, a range that tracks 2 stdevs above and 2 stdevs below the current 20 day simple moving average
 def addBolingerBands(df):
     SMAindex = check20SMA(df)
 
@@ -65,6 +74,7 @@ def addBolingerBands(df):
         df.iloc[i, df.shape[1]-2] = df.iloc[i, SMAindex] - 2 * std
         df.iloc[i, df.shape[1]-1] = df.iloc[i, SMAindex] + 2 * std
 
+#Adds the security's price's 20 day standard deviation
 def addSTD(df):
     df.loc[:,"STD"] = 0
     for i in range (STD_Period-1, len(df)):
@@ -73,19 +83,19 @@ def addSTD(df):
             lastPeriodClose[j] = df.iloc[i-j, 0]
         df.iloc[i, df.shape[1]-1] = np.std(lastPeriodClose)
 
-    
+
+#Adds the security's price's z score using the 20 day SMA and stdevs
 def addZScore(df):
     SMAindex = check20SMA(df)
-    
     df.loc[:,"ZScore"] = 0
-    for i in range (19, len(df)):
-        last20Close = np.zeros(20)
-        for j in range(20):
+    for i in range (STD_Period-1, len(df)):
+        last20Close = np.zeros(STD_Period)
+        for j in range(STD_Period):
             last20Close[j] = df.iloc[i-j, 0]
         std = np.std(last20Close)
         df.iloc[i, df.shape[1]-1] = (df.iloc[i, 0] - df.iloc[i, SMAindex]) / std
 
-        
+#Adds Moving Average Convergenve Divergence Indicator and its position to its signal line
 def addMACDandSignal(df):
     EMA12 = np.zeros(len(df))
     sum = 0
@@ -120,35 +130,72 @@ def addMACDandSignal(df):
     for i in range(34, len(df)):
        	avg = df.iloc[i, df.shape[1]-1] * 2 / 10 + avg * (1 - 2 / 10)
        	signal[i] = avg
-    df.loc[:,"MACD Signal Line"] = 0
+    df.loc[:,"MACD_Signal_Line"] = 0
     for i in range(len(df)):
        	df.iloc[i, df.shape[1]-1] = signal[i]
 
+#Adds the price the security will close at tomorrow, a potential label.
 def addNextClose(df):
-    df.loc[:, "Next Close"] = 0
+    df.loc[:, "Next_Close"] = 0
     for i in range(0, len(df)-1):
         df.iloc[i, df.shape[1]-1] = df.iloc[i+1, 0]
 
+#Adds the amount the security will change by tomorrow, a potential labe.
+def addDaysChange(df):
+    df["Days_Change"] = df["Next_Close"] - df["Close"]
 
-ticker = input("Filename: ")
-df = pd.read_csv(ticker + '.csv', index_col='Date', parse_dates=True, usecols=['Date', 'Close', 'Volume'], na_values='nan')
+#Adds a boolean feature as to whether the price of the security will go up
+#by the next day's close. Potential label for classification.
+def addGreenRed(df):
+    df["GreenRed"] = df["Days_Change"].apply(isPositive)
 
-df = df.rename(columns={'Close': ticker})
-df.dropna(inplace=True)
+def isPositive(n):
+    if n > 0:
+        return 1
+    else:
+        return 0
 
-addNextClose(df)
-addRSI(df)
-add20SMA(df)
-addBolingerBands(df)
-addSTD(df)
-addZScore(df)
-addMACDandSignal(df)
 
-ticker = ticker + '_features.csv'
-df.to_csv(ticker)
-ax = df.plot(title = '{} Price'.format(ticker))
-ax.set_xlabel('Date')
-ax.set_ylabel('RSI')
+#Adds all features to the dataframe. Some features are based on others, so
+#the order these functions are called are somewhat important.
+def addAllFeatures(df):
+    df.dropna(inplace=True)
+    df.rename(columns={ticker: "Close"})
+    addNextClose(df)
+    addRSI(df)
+    add20SMA(df)
+    addBolingerBands(df)
+    addSTD(df)
+    addZScore(df)
+    addMACDandSignal(df)
+    addDaysChange(df)
+    addGreenRed(df)
 
-ax.grid()
-plt.show()
+def plot(df):
+    ax = df.plot(title = '{} Price'.format(ticker))
+    ax.set_xlabel('Date')
+    ax.set_ylabel('RSI')
+    ax.grid()
+    plt.show()
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Incorrect usage, please pass in ticker name as only argument.")
+        sys.exit(1)
+    
+    ticker = sys.argv[1]
+    df = pd.read_csv(ticker + '.csv', index_col='Date', parse_dates=True, usecols=['Date', 'Close', 'Volume'], na_values='nan')
+
+    addAllFeatures(df)
+    
+#    plot(df)
+
+    df.to_csv(ticker + '_features.csv')
+    print("All features generated and exported as csv for " + ticker + " as " + ticker + '_features.csv' + ".")
+
+main()
+
+
+
+
